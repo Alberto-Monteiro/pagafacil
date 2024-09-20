03214dc2073aa03986b7330852989c8e547b9fb6
Alberto Monteiro <alberto.costa.monteiro@gmail.com>
2024-09-20 12:07:10 -0300

    Início

---
+++ src/main/java/com/rocksti/pagafacil/PagafacilApplication.java
@@ -0,0 +1,13 @@
+package com.rocksti.pagafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagafacilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagafacilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rocksti/pagafacil/PagafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocksti.pagafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagafacilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
